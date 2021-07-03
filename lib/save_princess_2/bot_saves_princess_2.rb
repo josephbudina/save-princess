91b837942d27b64373cc1b56ae213afd58ad650c
Joseph Budina <joebud3@gmail.com>
2021-07-03 11:44:39 -0600

    Add bot saves princess2 solution

---
+++ lib/save_princess_2/bot_saves_princess_2.rb
@@ -0,0 +1,34 @@
+class BotSavesPrincess2
+  attr_reader :bot_coords,
+              :princess_coords
+
+  def initialize(row, column, grid)
+    @bot_coords = [row, column]
+    @princess_coords = find_princess_position(grid)
+  end
+
+  def find_princess_position(grid)
+    grid.each_with_index do |line, index|
+      return [index, line.chars.index('p')] if line.include?('p')
+    end
+  end
+
+  def distance_to_princess
+    horizontal_distance = @princess_coords[1] - @bot_coords[1]
+    vertical_distance = @princess_coords[0] - @bot_coords[0]
+    [horizontal_distance, vertical_distance]
+  end
+
+  def next_move
+    distance = distance_to_princess
+    if distance[0] > 0
+      puts 'RIGHT'
+    elsif distance[0] < 0
+      puts 'LEFT'
+    elsif distance[1] > 0
+      puts 'DOWN'
+    elsif distance[1] < 0
+      puts 'UP'
+    end
+  end
+end
