9c5234fc64a96f9b03a8af80f461e542f89e8adb
Joseph Budina <joebud3@gmail.com>
2021-07-03 13:43:23 -0600

    Add more robust tests for both problems

--- spec/save_princess_2_spec/bot_saves_princess_2_spec.rb
+++ spec/save_princess_2_spec/bot_saves_princess_2_spec.rb
@@ -2,7 +2,7 @@ require './spec_helper'
 require 'save_princess_2/bot_saves_princess_2'
 
 describe 'Bot Saves Princess' do
-  it 'should instantiate with correct x/y coordinates for bot' do
+  it 'should find coords for bot and princess' do
     bot = BotSavesPrincess2.new(2, 3, ["---", "-m-", "p--"])
 
     expect(bot.bot_coords).to eq([2, 3])
@@ -13,5 +13,37 @@ describe 'Bot Saves Princess' do
     bot = BotSavesPrincess2.new(2, 3, ["---", "-m-", "p--"])
 
     expect(bot.distance_to_princess).to eq([-3, 0])
+  end
+
+  it 'should find next move left' do
+    bot = BotSavesPrincess2.new(2, 3, ["---", "-m-", "p--"])
+
+    expect do
+      bot.next_move
+    end.to output("LEFT\n").to_stdout
+  end
+
+  it 'should find next move right' do
+    bot = BotSavesPrincess2.new(1, 1, ["---", "--p", "---"])
+
+    expect do
+      bot.next_move
+    end.to output("RIGHT\n").to_stdout
+  end
+  
+  it 'should find next move up' do
+    bot = BotSavesPrincess2.new(1, 1, ["-p-", "---", "---"])
+
+    expect do
+      bot.next_move
+    end.to output("UP\n").to_stdout
+  end
+  
+  it 'should find next move down' do
+    bot = BotSavesPrincess2.new(1, 1, ["---", "---", "-p-"])
+
+    expect do
+      bot.next_move
+    end.to output("DOWN\n").to_stdout
   end
 end
